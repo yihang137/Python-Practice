54e7d163df8455ec3975bb089f54249dbd1059f6
yihang137 <yangyi@wsc.school.nz>
2025-05-22 11:08:28 +1200

    1

--- milestone_2/main.py
+++ milestone_2/main.py
@@ -5,3 +5,23 @@ name = input("Hi! What's your name? ")
 age = input("How old are you? ")
 
 print("Nice to meet you, " + name + "! You are " + age + " years old.")
+
+print("Hello! Welcome to this little program :)")
+
+hobby = input("What do you like?: ")
+
+print("Wow, " + hobby + " sounds interesting!")
+print("I'd love to learn more about " + hobby + ".")
+print("Thanks for sharing! Have a great day!")
+
+a = 10
+b = 3
+
+print("Given a = 10 and b = 3:")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+print("a // b =", a // b)
