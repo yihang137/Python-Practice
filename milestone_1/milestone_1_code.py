45929b9fc3958320773d04e43fef4cf7fa445128
yihang137 <yangyi@wsc.school.nz>
2025-05-22 11:28:57 +1200

    1

---
+++ milestone_1/milestone_1_code.py
@@ -0,0 +1,7 @@
+print(" hello ")
+
+name = input("Hi! What's your name? ")
+
+age = input("How old are you? ")
+
+print("Nice to meet you, " + name + "! You are " + age + " years old.")
---
+++ milestone_2/milestone_2_code.py
@@ -0,0 +1,22 @@
+
+print("Hello! Welcome to this little program :)")
+
+hobby = input("What do you like?: ")
+
+if hobby.lower() == "math":
+    print("Awesome! Math is a great way to train your brain.")
+else:
+    print("Wow, " + hobby + " sounds interesting!")
+    print("I'd love to learn more about " + hobby + ".")
+
+a = 10
+b = 3
+
+print("Given a = 10 and b = 3:")
+print("a + b =", a + b)
+print("a - b =", a - b)
+print("a * b =", a * b)
+print("a / b =", a / b)
+print("a % b =", a % b)
+print("a ** b =", a ** b)
+print("a // b =", a // b)
